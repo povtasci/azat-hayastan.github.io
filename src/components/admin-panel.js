4b65b821c8c79cce7d9dbc90eb09c59de28a514d
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-04-19 01:31:34 -0700

    Start admin panel

---
+++ src/components/admin-panel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+import styles from './admin-panel.module.css';
+
+export default class Admin extends React.Component {
+  render() {
+    return <p>hi</p>;
+  }
+}
