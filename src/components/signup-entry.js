7f266d1e5cd447d983373b1d7f8616110f32afd9
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-04-19 00:57:11 -0700

    More simplifications

--- src/components/signup-entry.js
+++ src/components/signup-entry.js
@@ -15,8 +15,6 @@ const SHARE_YOUR_THOUGHTS_LIMIT = 3000;
 const INIT_STATE = {
   error: null,
   signup_phone_number: '',
-  signup_password: '',
-  share_your_thoughts: '',
   captcha_satisfied: false,
 };
 
@@ -25,24 +23,13 @@ export default class FormEntry extends React.Component {
 
   on_submit = e => {
     e.preventDefault();
-    const {
-      signup_phone_number,
-      signup_password,
-      share_your_thoughts,
-      captcha_satisfied,
-    } = this.state;
+    const { signup_phone_number, share_your_thoughts, captcha_satisfied } = this.state;
     const { on_submit_signup } = this.props;
     const is_valid_phone_number = is_phone_number(signup_phone_number);
-    const signup_password_more_than_five = signup_password.length >= 5;
-    const user_thoughts = share_your_thoughts.slice(0, SHARE_YOUR_THOUGHTS_LIMIT);
-    if (
-      is_valid_phone_number === false ||
-      signup_password_more_than_five === false ||
-      captcha_satisfied === false
-    ) {
+    if (is_valid_phone_number === false || captcha_satisfied === false) {
       this.setState(() => ({ error: new Error(`Phone number or password is not valid`) }));
     } else {
-      on_submit_signup({ signup_phone_number, signup_password, user_thoughts });
+      on_submit_signup({ signup_phone_number });
     }
   };
 
@@ -51,9 +38,7 @@ export default class FormEntry extends React.Component {
   }
 
   set_phone_number = ({ intlPhoneNumber }) =>
-    this.setState({
-      signup_phone_number: intlPhoneNumber,
-    });
+    this.setState({ signup_phone_number: intlPhoneNumber });
 
   recaptcha_changed = data => data !== null && this.setState(() => ({ captcha_satisfied: true }));
 
@@ -69,29 +54,14 @@ export default class FormEntry extends React.Component {
             onChange={this.set_phone_number}
           />
           {SPACER_15_H}
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            type={'password'}
-            value={this.state.signup_password}
-            placeholder={'Do not lose this password'}
-            onChange={e => this.setState({ signup_password: e.target.value })}
-          />
-          <FormControl.Feedback />
-          <HelpBlock>Password must be at least five characters</HelpBlock>
-          <FormGroup controlId="formControlsTextarea">
-            <ControlLabel>Optional Message</ControlLabel>
-            <FormControl
-              value={this.state.share_your_thoughts}
-              componentClass={'textarea'}
-              onChange={e => this.setState({ share_your_thoughts: e.target.value })}
-              placeholder={'share your thoughts'}
+          <div className={styles.ReCAPTCHA__Wrapper}>
+            <ReCAPTCHA
+              ref="recaptcha"
+              sitekey={RECAPTCHA_SITE_KEY}
+              onChange={this.recaptcha_changed}
             />
-          </FormGroup>
-          <ReCAPTCHA
-            ref="recaptcha"
-            sitekey={RECAPTCHA_SITE_KEY}
-            onChange={this.recaptcha_changed}
-          />,
+          </div>
+          {SPACER_15_H}
           <Button block={true} bsSize={'large'} bsStyle={'primary'} type={'submit'}>
             Subscribe
           </Button>
